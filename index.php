74fde27202503a43e48718c713a9d175ef833a5d
DanialRj <danialfc40@gmail.com>
2019-03-12 20:15:41 +0700

    add index

---
+++ index.php
@@ -0,0 +1,25 @@
+</<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Hacktive CoHive</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+</head>
+<body>
+    <h1>This is a heading</h1>
+    <p>This is a Paragraf</p>
+
+    <form>
+        First name :<br>
+        <input type="text" name="FirstName"><br>
+        Last Name :<br>
+        <input type="text" name="LastName"><br>
+    </form>
+
+    dadadada
+
+</body>
+<script src="js/script.js"></script>
+</html> 
